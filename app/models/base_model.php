ecd80c7bc5f56ac656e3a96ef180bfdc94d37952
Thayol <thayol@zovguran.net>
2022-02-12 13:12:57 +0100

    Added Model->save()

--- app/models/base_model.php
+++ app/models/base_model.php
@@ -13,6 +13,47 @@ class DatabaseRecord
 
     public function destroy() {
         $table = static::table;
-        Query::new(static::class)->delete()->where("id = ?", $this->id)->commit();
+        return Query::new(static::class)->delete($this->id)->commit() > 0;
+    }
+
+    public function save() {
+        $values = array();
+        foreach (static::fields as $field)
+        {
+            $values[] = $this->$field;
+        }
+
+        $values = array_slice($values, 1);
+
+        return Query::new(static::class)->update($this->id)->values($values)->commit() > -1;
+    }
+
+    public static function query() {
+        return Query::new(static::class);
+    }
+
+    public static function insert() {
+        return static::query()->insert();
+    }
+
+    public static function select($fields = null) {
+        if (empty($fields))
+        {
+            $fields = static::fields;
+        }
+
+        return static::query()->select();
+    }
+
+    public static function update($id) {
+        return static::query()->update($id);
+    }
+
+    public static function delete($id) {
+        return static::query()->delete($id);
+    }
+
+    public static function find($id) {
+        return static::query()->find($id);
     }
 }
