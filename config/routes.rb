63a98fd8828f3ea936e433493759327468b79bb1
Thayol <thayol@zovguran.net>
2022-11-18 17:01:57 +0100

    fixed the logout styling

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   post 'signup_handler', to: 'auth#signup_handler'
   get 'logout', to: 'auth#logout'
 
-
   get 'profiles', to: 'profiles#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
